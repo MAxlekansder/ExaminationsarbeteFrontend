751fe86b40038ab9556e8a98630c5af6b4c55b85
Kbidros <kristianbidros@gmail.com>
2024-03-06 22:41:30 +0100

    adding drop down menu and icons

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -1,18 +1,25 @@
+// Kristian made this file
 
+import { VscAccount } from "react-icons/vsc";
 
 const NavBarComponent = () => {
     return (
-        <nav className="navbar">
-            <ul className="nav-list">
-                <li><a href="/" className="nav-link">Hem</a></li>
-                <li><a href="/" className="nav-link">Kategorier</a></li>
-                <li><a href="/" className="nav-link">Veckans tips</a></li>
-                <li><a href="/" className="nav-link">Om oss</a></li>
-                <li><a href="/" className="nav-profile">Profilikon</a></li>
-
-            </ul>
-        </nav>
-
+        <header>
+            <div className="logo">
+                <a href="/"><img src="./Images/logo1" alt="" /></a>
+            </div>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li><a href="/" className="nav-link">Hem</a></li>
+                    <li><a href="/" className="nav-link">Kategorier</a></li>
+                    <li><a href="/" className="nav-link">Veckans tips</a></li>
+                    <li><a href="/" className="nav-link">Om oss</a></li>
+                </ul>
+            </nav>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+                <VscAccount className="login-icon" />
+            </a>
+        </header>
     )
 }
 
