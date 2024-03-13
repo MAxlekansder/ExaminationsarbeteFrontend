9a7821cc3738a7518d0f4022cf99e27b870ca002
mehdinoori20 <145023240+mehdinoori20@users.noreply.github.com>
2024-03-13 15:12:45 +0100

    deleted dishdetailspage and fixed the rest

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import './NavBar.css'
+import './NavBar.css';
+import './Dishes.css';
 import { useState, useEffect } from 'react';
 import RecipeSearch from './components/RecipeSearchProps';
 import NavBarComponent from './components/NavBarComponent'
@@ -8,7 +9,6 @@ import HomePage from './pages/HomePage';
 import DishComponent from './components/DishComponent';
 import dishes from './data/Dishes';
 import { Route, Routes } from 'react-router-dom';
-import DishDetailsPage from './pages/DishDetailsPage';
 
 
 function App() {
@@ -35,25 +35,25 @@ function App() {
           recipesFromInterface={recipes}
           searchTerm={searchTerm}
           onSearchChange={handleSearchChange} />
+        
            <Routes>
-      <Route path="/dishes/:id" element={<DishDetailsPage />} />
-        <Route path='/' element={<HomePage />} />
-        <Route path='/dishes' element={(
-          <div>
-            {dishes.map((dish, index) => (
-              <DishComponent
-                id={dish.id}
-                key={index}
-                name={dish.name}
-                image={dish.image}
-                recipe={dish.recipe}
-                ingredients={dish.ingredients}
-                onClick={() => console.log(dish.recipe)} // Eller annan logik för att visa receptet.
-              />
-            ))}
-          </div>
-        )} />
-      </Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/dishes' element={(
+              <div>
+                {dishes.map((dish, index) => (
+                  <DishComponent
+                    id={dish.id}
+                    key={index}
+                    name={dish.name}
+                    image={dish.image}
+                    recipe={dish.recipe}
+                    ingredients={dish.ingredients}
+                    onClick={() => console.log(dish.recipe)} // Eller annan logik för att visa receptet.
+                  />
+                ))}
+              </div>
+            )} />
+          </Routes>
       </div>
     </div>
   );
