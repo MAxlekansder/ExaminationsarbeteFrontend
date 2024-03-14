36ae9c41357ee40d0a378cb02b96a19112642069
MAxlekansder <alexander.hasslund@stud.sti.se>
2024-03-14 16:42:25 +0100

    saving changes

--- src/components/Cocktails/CocktailComponent.tsx
+++ src/components/Cocktails/CocktailComponent.tsx
@@ -2,42 +2,39 @@
 
 import { useEffect, useState } from "react";
 import useRecipeState from "../../State/indexState";
-
-
-
+import NavBarComponent from "../NavBarComponent";
 
 function PresentCocktails() {
-    const getRecipe = useRecipeState((state) => state.fetchRecipe);
-    const getDrinks = useRecipeState((state) => state.fetchDrinks);
-    const drinks = useRecipeState((state) => state.drinks);
-    const recipes = useRecipeState((state) => state.recipes);
+  const getDrinks = useRecipeState((state) => state.fetchDrinks);
+  const drinks = useRecipeState((state) => state.drinks);
 
-    useEffect(() => {
-        getDrinks();
-    }, [getDrinks])
+  useEffect(() => {
+    getDrinks();
+  }, [getDrinks]);
 
-
-
-    return(
-        <div>
-        <p className="text-xl font-bold mb-4">tänkbara drinkar</p>
-        <div className="flex flex-wrap -mx-2">
-            {drinks.slice(0,10).map((drink) => (
-                <div key={drink.idDrink} className="w-1/3 px-2 mb-4">
-                    <div className="bg-white p-4 shadow-md">
-                        <p className="text-lg font-semibold">
-                        {drink.strDrink}, 
-                        {drink.strAlcoholic}, 
-                        {drink.strCategory},
-                        {drink.strIngredient1}
-                        </p>
-                        <img src={drink.strDrinkThumb} alt={drink.strDrink} className="w-full h-auto mb-2" />
-                    </div>
-                </div>
-            ))}
-        </div>
+  return (
+    <div>
+      <NavBarComponent/>
+      <h1 className="text-xl font-bold mb-4">tänkbara drinkar</h1>
+      <div className="flex flex-wrap -mx-2">
+        {drinks.slice(0, 9).map((drink) => (
+          <div key={drink.idDrink} className="w-1/3 px-2 mb-4">
+            <div className="bg-white p-4 shadow-md">
+              <p className="text-lg font-semibold">
+                {drink.strDrink},{drink.strAlcoholic},{drink.strCategory},
+                {drink.strIngredient1}
+              </p>
+              <img
+                src={drink.strDrinkThumb}
+                alt={drink.strDrink}
+                className="w-full h-auto mb-2"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-    );
+  );
 }
 
 export default PresentCocktails;
