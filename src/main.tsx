74dcc9913a83277c441a28ddaa4f4f9c0fd37ad3
Kbidros <kristianbidros@gmail.com>
2024-03-15 13:33:41 +0100

    formatting

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './index.css'
+import './Styling/index.css'
 import HandleRequests from './components/AddRecipe/HandleRequestComponent.tsx';
 import Home from "./Views/Home/Home.tsx";
 import SubPage from "./Views/SubPage/SubPage.tsx";
@@ -16,18 +16,18 @@ import DetailedCocktailComponent from './components/Cocktails/DetailedCocktailCo
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<App />} />
-    <Route path='/main-page' element="null" />
-    <Route path='/add' element={<HandleRequests />} />
-    <Route path='/home' element={<Home/>} />
-    <Route path='/cocktails' element={<PresentCocktails/>} />
-    <Route path='/cocktails/:id' element=<DetailedCocktailComponent/> />
-    <Route path='/subpage' element={<SubPage/>} />
-    <Route path='/recipe1' element={<Recipe title={"Köttbullar"} description={"Gör så här..."}/>} />
-    <Route path='/recipe2' element={<Recipe title={"Pizza"} description={"Gör så här..."}/>} />
-    <Route path='/recipe3' element={<Recipe title={"Kebab"} description={"Gör så här..."}/>} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/main-page' element="null" />
+        <Route path='/add' element={<HandleRequests />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/cocktails' element={<PresentCocktails />} />
+        <Route path='/cocktails/:id' element=<DetailedCocktailComponent /> />
+        <Route path='/subpage' element={<SubPage />} />
+        <Route path='/recipe1' element={<Recipe title={"Köttbullar"} description={"Gör så här..."} />} />
+        <Route path='/recipe2' element={<Recipe title={"Pizza"} description={"Gör så här..."} />} />
+        <Route path='/recipe3' element={<Recipe title={"Kebab"} description={"Gör så här..."} />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {NavLink} from "react-router-dom";
+
+
+const Home = () => {
+    return (
+        <div>
+
+
+           <br/><br/><form action="/action_page.pgp">
+            <label htmlFor="fname"> First name: </label>
+            <input type="text" id="fname" name="fname"/><br/><br/>
+            <label htmlFor="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"/><br/><br/>
+            <input type="submit" value="Submit"/>
+
+        </form>
+            <h1>Home</h1>
+            <br/><NavLink style={{backgroundColor: "yellow", fontWeight: "bold", fontStyle: "italic", padding: 5, borderRightStyle: "solid", borderRightWidth: 2}}
+                     to="/recipe1">Köttbullar</NavLink><br/><br/>
+            <br/><NavLink style={{backgroundColor: "yellow", fontWeight: "bold", fontStyle: "italic", padding: 5, borderRightStyle: "solid", borderRightWidth: 2}}
+                     to="/recipe2">Pizza</NavLink><br/><br/>
+            <br/><NavLink style={{backgroundColor: "yellow", fontWeight: "bold", fontStyle: "italic", padding: 5, borderRightStyle: "solid", borderRightWidth: 2}}
+                     to="/recipe3">Kebab</NavLink><br/><br/>
+        </div>
+)
+}
+export default Home
+
+
