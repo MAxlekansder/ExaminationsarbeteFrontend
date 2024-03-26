ca2f62fa80d9f6b0a5a7386a70d3dd101e56a180
MAxlekansder <alexander.hasslund@stud.sti.se>
2024-03-26 14:05:25 +0100

    fixed modal first version

--- src/components/AddRecipe/HandleRequest.tsx
+++ src/components/AddRecipe/HandleRequest.tsx
@@ -150,7 +150,6 @@
                   <CategorySelected 
                     selectedCategories={categories}
                     onChange={setCategories}
-                    
                   />
                 </div>
               </div>
