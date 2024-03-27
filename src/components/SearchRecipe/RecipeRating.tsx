02f1b16b70ed65ab6cea53f7522ce20767ccf00d
Sertan <sertan.keskin@stud.sti.se>
2024-03-27 22:59:59 +0100

    update RecipeRating.tsx & TestDetail.tsx

--- src/components/SearchRecipe/RecipeRating.tsx
+++ src/components/SearchRecipe/RecipeRating.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-interface RecipeRatingProps {
+interface RecipeRating {
   dishId: string;
   rating: number | null;
   setRating: (rating: number) => void;
 }
 
-const RecipeRating: React.FC<RecipeRatingProps> = ({ dishId, rating, setRating }) => {
+
+  const RecipeRating: React.FC<RecipeRating> = ({ dishId, rating, setRating }) => {
+  const [comment, setComment] = useState('');
+
   const handleRatingChange = (newRating: number) => {
-   
     console.log(`Rated dish ${dishId} with ${newRating} stars`);
     setRating(newRating);
+  };
+
+  const handleCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(event.target.value);
   };
 
   return (
@@ -20,6 +26,14 @@ const RecipeRating: React.FC<RecipeRatingProps> = ({ dishId, rating, setRating }
           {value} {rating === value && '★'} 
         </button>
       ))}
+      <textarea
+        placeholder="Enter your comment here.."
+        value={comment}
+        onChange={handleCommentChange}
+        />
+      <button onClick={() => console.log(`Comment for dish ${dishId}: ${comment}`)}>
+
+      </button>
     </div>
   );
 };
