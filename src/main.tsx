e51c5942dfecb7488dd0149fbe5f157626c90b93
Kbidros <kristianbidros@gmail.com>
2024-04-03 21:52:57 +0200

    shoppingcart updated

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './Styling/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
 )
 
--- src/pages/DetailedRecipe/DetailedRecipe.tsx
+++ src/pages/DetailedRecipe/DetailedRecipe.tsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from 'react'
-import {useParams,} from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { useParams, } from "react-router-dom";
 import axios from "axios";
 import Description from "../../Views/Recipe/Components/Description/Description.tsx";
 import Title from "../../Views/Recipe/Components/Title/Title.tsx";
 
 
 const DetailedRecipe = () => {
-    const {id} = useParams<{ id: string }>(); //* Hämtar Id från URL
+    const { id } = useParams<{ id: string }>(); //* Hämtar Id från URL
     const [recipe, setRecipe] = useState<any>() //* Sparar resultatet av det vi hämtar
 
     const getRecipe = () => { //* Hämtar data ifrån backend
@@ -32,8 +32,8 @@ const DetailedRecipe = () => {
         <div>
             <>
                 <h1>Recipe</h1>
-                <Title title={recipe.title}/>
-                <Description timeInMins={recipe.timeInMins} categories={recipe.categories} instructions={recipe.instructions} ingredients={recipe.ingredients} description={recipe.description}imageUrl={recipe.imageUrl}/>
+                <Title title={recipe.title} />
+                <Description timeInMins={recipe.timeInMins} categories={recipe.categories} instructions={recipe.instructions} ingredients={recipe.ingredients} description={recipe.description} imageUrl={recipe.imageUrl} />
             </>
         </div>
     )
