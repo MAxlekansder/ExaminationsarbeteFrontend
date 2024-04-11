6e96781c45d4ac452d819be966a41da3160b5437
SimonGrFo <simgrafor03@gmail.com>
2024-04-11 12:58:57 +0200

    footer edited

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,20 +1,38 @@
-//Component by Gustavo
+//Component by Gustavo 
+// stulet av simon
 
-const FooterComponent = () => {
+
+import React from 'react';
+
+const FooterComponent: React.FC = () => {
   return (
-    <div>
-      <div className='bg-green-900 '>
-        <footer>
-          <p className='text-white text-center 2xl:text-xs '>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore expedita non, aperiam ipsam pariatur ab, fugiat facilis mollitia assumenda similique beatae vel nulla! Culpa reiciendis quidem, voluptatem unde modi quia.
-          </p>
-          <h1 className='flex justify-start pt-20  text-white'>
-            Copyright © 2024 Recept.nu . All Rights Reserved
-          </h1>
-        </footer>
-      </div>
+    <div className="bg-green-900">
+      <footer className="container mx-auto py-4 px-4 flex justify-between items-center">
+        <div className="text-white text-sm md:text-base">
+          Stay connected with us:
+          <div className="mt-2">
+            <button className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2">
+              Facebook
+            </button>
+            <button className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2">
+              Twitter
+            </button>
+            <button className="bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+              Instagram
+            </button>
+          </div>
+        </div>
+        <div className="text-white text-xs md:text-sm">
+          <ul>
+            <li>E-mail: help@recipes.se</li>
+            <li>Phone: +08-000 000 06</li>
+            <li>Address: chillington, 7, Stockholm</li>
+          </ul>
+          <p>&copy; 2024 Recept.nu. All Rights Reserved</p>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
